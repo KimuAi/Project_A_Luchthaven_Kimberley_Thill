328f2eac6bcfd765323cc4c5378a963ccab728bd
Kimberley Thill <ThillKimberleyPro@gmail.com>
2025-01-06 18:13:31 +0100

    Persoon klasse aangemaakt

---
+++ src/Persoon.java
@@ -0,0 +1,15 @@
+public class Persoon {
+
+        //Gegevens van de persoon
+        String naam;
+        int leeftijd;
+        String addres;
+
+        // definieer mijn constructor voor klasse Persoon (nieuw object)
+        public Persoon(String name, int age, String address) {
+            this.naam = name;
+            this.leeftijd = age;
+            this.addres = address;
+        }
+    }
+
