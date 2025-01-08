9e2d16b8e066230d3eaf18a19e13476973aeadae
Kimberley Thill <ThillKimberleyPro@gmail.com>
2025-01-08 14:40:48 +0100

    Eind product!

--- src/Vliegtuig.java
+++ src/Vliegtuig.java
@@ -2,13 +2,16 @@
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Vliegtuig {
+    // Teller voor unieke vliegtuigcodes
     private static final AtomicInteger counter = new AtomicInteger(1);
+
     private String vliegtuigCode;
     private String vertrekLocatie;
     private String bestemming;
     private int economyStoelen;
     private int businessStoelen;
 
+    // Constructor om een vliegtuig te creëren met gegeven gegevens
     public Vliegtuig(String vertrekLocatie, String bestemming, int economyStoelen, int businessStoelen) {
         this.vliegtuigCode = generateVliegtuigCode();
         this.vertrekLocatie = vertrekLocatie;
@@ -17,18 +20,21 @@ public class Vliegtuig {
         this.businessStoelen = businessStoelen;
     }
 
+    // Maakt een vliegtuigcode (VL001, VL002, ..)
     private String generateVliegtuigCode() {
         return "VL" + String.format("%03d", counter.getAndIncrement());
     }
 
+    // Reserveert een economy stoel, als beschikbaar
     public boolean reserveerEconomy() {
-        if (economyStoelen > 0) {
+        if (economyStoelen > 0) {  // Controleer of er economy stoelen beschikbaar zijn
             economyStoelen--;
             return true;
         }
-        return false;
+        return false; // Geen stoelen beschikbaar
     }
 
+    // Reserveert een business stoel, als er beschikbaar zijn
     public boolean reserveerBusiness() {
         if (businessStoelen > 0) {
             businessStoelen--;
@@ -37,6 +43,7 @@ public class Vliegtuig {
         return false;
     }
 
+    // Geeft de unieke vliegtuigcode terug
     public String getVliegtuigCode() {
         return vliegtuigCode;
     }
